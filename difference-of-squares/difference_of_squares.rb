9c5b6b6004541ca9422b747d308716730518c8c2
genekhanutin <genekhanutin@gmail.com>
2015-05-23 13:13:55 -0600

    all tests pass

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -11,13 +11,8 @@ class Squares
   def sum_of_squares
     (0..@number).to_a.reduce(0) {|total, number| total + (number ** 2)}
   end
+
+  def difference
+    square_of_sums - sum_of_squares
+  end
 end
-
-
-1**2 + 2**2 + 3**2 + 4**2 + 5**2
-
-# SQUARE OF SUMS
-# start with counter at 0
-# iterate from 0 to number
-# for each iteration add number to counter
-# take final sum of counter and square that number
